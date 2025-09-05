ac3011b7018bf3a234b703a2fa6a57db94469c3b
suman Mahat <xetrisuman12345@gmail.com>
2025-09-05 19:46:34 +0930

    improve input validation by user

--- ques1/program.py
+++ ques1/program.py
@@ -1,6 +1,17 @@
 '''Program that reads the text file "raw_text.txt", encrypts its contents using a simple encryption method, and writes the encrypted text to a new file
 "encrypted_text.txt". Then create a function to decrypt the content and a function to verify the decryption was successful '''
 
+
+
+# Function to validate input entered by users
+def get_valid_input(prompt):
+    while True:
+        value = input(prompt)   # get input by user
+        if value.isdigit():    # check whether the input is valid number or not
+            return int(value)
+        print("Invalid input, Please enter a number only")  # if user enter invalid input, ask to enter a valid number
+
+
 # Function to shift a letter forward within start-end range with wrap-around.
 def shift_forward(char, shift_amount, start, end):
     new_ord = ord(char) + shift_amount  #shift letter forward
@@ -61,15 +72,6 @@ def verify_decrypt(original_text, decrypted_text):
         print("Decryption is successful. Raw texts and decrypted texts match.")
     else:
         print("Decryption is failed. Raw texts and decrypted texts doesn't match")
-
-
-# Function to validate input entered by users
-def get_valid_input(prompt):
-    while True:
-        value = input(prompt)   # get input by user
-        if value.isdigit():    # check whether the input is valid number or not
-            return int(value)
-        print("Invalid input, Please enter a number only")  # if user enter invalid input, ask to enter a valid number
 
 
 # Main program
