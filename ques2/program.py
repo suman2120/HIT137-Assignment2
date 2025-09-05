642845e7e05c6b71d65c98f781221d0e883cbce9
suman Mahat <xetrisuman12345@gmail.com>
2025-09-05 20:52:59 +0930

    modified ques 2 for csv file location

--- ques2/program.py
+++ ques2/program.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob  #used to get all csv files automatically
 
 #Read all CSV files from "temperatures" folder
-all_csv_files = glob.glob("temperatures/*.csv") 
+all_csv_files = glob.glob("ques2/temperatures/*.csv") 
 
 all_dataframes = []  # temporary list to store each csv as a dataframe
 
@@ -30,7 +30,7 @@ seasonal_avg = {}
 for season, month_list in seasons.items():
     seasonal_avg[season] = data[month_list].stack().mean()  #average per season across all stations
 
-with open('average_temp.txt','w') as f:  
+with open('ques2/average_temp.txt','w') as f:  
     for season, avg in seasonal_avg.items():
         f.write(f'{season}: {avg:.2f} degree celsius\n') # save seasonal average in required format
 
@@ -44,7 +44,7 @@ max_range = data['TemperatureRange'].max()
 largest_range_stations = data[data['TemperatureRange'] == max_range] # get stations with the largest range
 
 
-with open('largest_temp_range_station.txt', 'w') as f:  
+with open('ques2/largest_temp_range_station.txt', 'w') as f:  
     for _, row in largest_range_stations.iterrows():
         f.write(
             f"Station {row['STATION_NAME']}: Range {row['TemperatureRange']:.2f} degree celsius "
@@ -61,7 +61,7 @@ most_stable = data[data['StandardDeviation'] == min_std] # get most stable stati
 
 most_variable = data[data['StandardDeviation'] == max_std] #get most variable stations
 
-with open('temperature_stability_station.txt','w') as f:   
+with open('ques2/temperature_stability_station.txt','w') as f:   
     for i, row in most_stable.iterrows():
         f.write(f"Most Stable: Station {row['STATION_NAME']}: StdDev {row['StandardDeviation']:.2f} degree celsius\n")
     for i, row in most_variable.iterrows():
